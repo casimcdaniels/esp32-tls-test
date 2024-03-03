ef7e2f97681df239651b9223c3ceed5006eb4861
Casi McDaniels <casimcdaniels@gmail.com>
2024-03-02 21:26:05 -0500

    working

--- src/main.rs
+++ src/main.rs
@@ -5,59 +5,38 @@
 use esp32c3_hal as hal;
 
 use embassy_net::tcp::TcpSocket;
-use embassy_net::{Config, Ipv4Address, Stack, StackResources};
+use embassy_net::{Config, Stack, StackResources};
 
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
 use esp_backtrace as _;
 use esp_mbedtls::X509;
 use esp_mbedtls::{asynch::Session, set_debug, Certificates, Mode, TlsVersion};
-use esp_println::logger::init_logger;
 use esp_println::{print, println};
 use esp_wifi::wifi::{
-    ClientConfiguration, Configuration, WifiController, WifiDevice, WifiEvent, WifiStaDevice,
-    WifiState,
+    ClientConfiguration, Configuration, WifiController, WifiDevice, WifiEvent,
+    WifiStaDevice, WifiState,
 };
 use esp_wifi::{initialize, EspWifiInitFor};
 use hal::clock::ClockControl;
 use hal::Rng;
 use hal::{embassy, peripherals::Peripherals, prelude::*, rsa::Rsa, timer::TimerGroup};
+use heapless::Vec;
+use rust_mqtt::client::client::MqttClient;
+use rust_mqtt::client::client_config::ClientConfig;
+use rust_mqtt::packet::v5::reason_codes::ReasonCode;
+use rust_mqtt::utils::rng_generator::CountingRng;
+use smoltcp::wire::DnsQueryType;
 use static_cell::make_static;
 
+const SSID: &str = env!("SSID");
+const PASSWORD: &str = env!("PASSWORD");
+const BROKER_URL: &str = env!("BROKER_URL");
+const PORT: &str = env!("PORT");
+const CLIENT_1_ID: &str = env!("CLIENT_1_ID");
+const DEVICE_1_USERNAME: &str = env!("DEVICE_1_USERNAME");
+const DEVICE_1_PASSWORD: &str = env!("DEVICE_1_PASSWORD");
 
-const SSID: &str = "Wokwi-GUEST"; //env!("SSID");
-const PASSWORD: &str = ""; // env!("PASSWORD");
-const CA: &str = r#"-----BEGIN CERTIFICATE-----\n"
-"MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw\n"
-"TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh\n"
-"cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4\n"
-"WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu\n"
-"ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY\n"
-"MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc\n"
-"h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+\n"
-"0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U\n"
-"A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW\n"
-"T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH\n"
-"B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC\n"
-"B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv\n"
-"KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn\n"
-"OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn\n"
-"jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw\n"
-"qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI\n"
-"rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV\n"
-"HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq\n"
-"hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL\n"
-"ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ\n"
-"3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK\n"
-"NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5\n"
-"ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur\n"
-"TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC\n"
-"jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc\n"
-"oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq\n"
-"4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA\n"
-"mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d\n"
-"emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=\n"
-"-----END CERTIFICATE-----\n"#;
 #[main]
 async fn main(spawner: Spawner) -> ! {
     esp_println::logger::init_logger(log::LevelFilter::Info);
@@ -131,7 +110,21 @@ async fn main(spawner: Spawner) -> ! {
 
         socket.set_timeout(Some(embassy_time::Duration::from_secs(10)));
 
-        let remote_endpoint = (Ipv4Address::new(52,58,181,123), 8883);
+        let address = match stack
+            .dns_query(BROKER_URL, DnsQueryType::A)
+            .await
+            .map(|a| a[0])
+        {
+            Ok(address) => address,
+            Err(e) => {
+                println!("DNS lookup error: {e:?}");
+                continue;
+            }
+        };
+
+        let port: u16 = PORT.parse().expect("Failed to parse PORT as u16");
+        let remote_endpoint = (address, port);
+
         println!("connecting...");
         let r = socket.connect(remote_endpoint).await;
         if let Err(e) = r {
@@ -140,48 +133,16 @@ async fn main(spawner: Spawner) -> ! {
         }
         println!("connected!");
 
+    
+        set_debug(0);
 
-        // embedded-tls example
-        
-        // This won't work with TLS 1.2 and below servers as it only handles 1.3 standard.
-
-        // let mut buf = [0; 1024];
-        // use embedded_io_async::Write;
-        // let mut read_record_buffer = [0; 16384];
-        // let mut write_record_buffer = [0; 16384];
-        // let config = TlsConfig::new()
-        //     .with_server_name("google.com")
-        //     .enable_rsa_signatures();
-        // let mut tls: TlsConnection<'_, TcpSocket<'_>, Aes128GcmSha256> =
-        //     TlsConnection::new(socket, &mut read_record_buffer, &mut write_record_buffer);
-
-        // match tls
-        //     .open::<Rng, NoVerify>(TlsContext::new(&config, &mut rng))
-        //     .await
-        // {
-        //     Ok(_) => {
-        //         println!("TLS connection successful")
-        //     }
-        //     Err(e) => {
-        //         println!("error establishing TLS connection: {:?}", e);
-        //     }
-        // }
-        
-
-        // esp-mbed-tls
-        // set_debug(0);
-
-        // Comment the below line and rebuild
         let tls: Session<_, 4096> = Session::new(
             &mut socket,
-            "broker.hivemq.com",
+            BROKER_URL,
             Mode::Client,
             TlsVersion::Tls1_2,
-            Certificates { 
-                ca_chain: X509::pem(
-                concat!(include_str!("../certs/ca.pem"), "\0").as_bytes(),
-            )
-            .ok(),
+            Certificates {
+                ca_chain: X509::pem(concat!(include_str!("../cert.pem"), "\0").as_bytes()).ok(),
                 ..Default::default()
             },
             Some(&mut rsa),
@@ -189,39 +150,93 @@ async fn main(spawner: Spawner) -> ! {
         .unwrap();
 
         println!("Start TLS connect");
-        let mut _tls = tls.connect().await.unwrap();
+        let mut tls = tls.connect().await.unwrap();
 
         println!("TLS connection established");
 
-        // HTTP request
-        // let r = socket
-        //     .write_all(b"GET / HTTP/1.0\r\nHost: www.mobile-j.de\r\n\r\n")
-        //     .await;
-        // if let Err(e) = r {
-        //     println!("write error: {:?}", e);
-        //     continue;
-        // }
-        // let n = match socket.read(&mut buf).await {
-        //     Ok(0) => {
-        //         println!("read EOF");
-        //         break;
-        //     }
-        //     Ok(n) => n,
-        //     Err(e) => {
-        //         println!("read error: {:?}", e);
-        //         break;
-        //     }
-        // };
-        // println!("{}", core::str::from_utf8(&buf[..n]).unwrap());
-    }
+        // MQTT Connection Setup
+        let mut config = ClientConfig::new(
+            rust_mqtt::client::client_config::MqttVersion::MQTTv5,
+            CountingRng(20000),
+        );
+        config.add_max_subscribe_qos(rust_mqtt::packet::v5::publish_packet::QualityOfService::QoS2);
+        config.add_client_id(CLIENT_1_ID.try_into().unwrap());
+        config.add_username(DEVICE_1_USERNAME.try_into().unwrap());
+        config.add_password(DEVICE_1_PASSWORD.try_into().unwrap());
+        config.max_packet_size = 4096;
+        let mut recv_buffer = [0; 4096];
+        let mut write_buffer = [0; 4096];
 
-    loop {}
+        println!("Attempting MQTT connection");
+        let mut client = MqttClient::<_, 5, _>::new(
+            &mut tls,
+            &mut write_buffer,
+            4096,
+            &mut recv_buffer,
+            4096,
+            config,
+        );
+
+        match client.connect_to_broker().await {
+            Ok(()) => {
+                println!("Connected to broker");
+            }
+            Err(mqtt_error) => match mqtt_error {
+                ReasonCode::NetworkError => {
+                    println!(
+                        "MQTT Network Error - Broker Connect Failure: {:?}",
+                        mqtt_error
+                    );
+                    continue;
+                }
+                _ => {
+                    println!("Other MQTT Error: {:?}", mqtt_error);
+                    continue;
+                }
+            },
+        }
+
+        let mut topics_to_subscribe: Vec<&str, 5> = Vec::new();
+        topics_to_subscribe.push("topicfeed").unwrap();
+
+        let subscription_result = client.subscribe_to_topics(&topics_to_subscribe).await;
+        match subscription_result {
+            Ok(_) => println!("Successfully subscribed to topics"),
+            Err(e) => {
+                println!("Failed to subscribe: {:?}", e);
+            }
+        };
+
+        // Receieve messages from MQTT broker
+        loop {
+            match client.receive_message().await {
+                Ok((topic, message)) => {
+                    let message_str = core::str::from_utf8(message).unwrap_or("<Invalid UTF-8>");
+                    println!("Received message on topic {:#?}: {:#?}", topic, message_str);
+                }
+                Err(mqtt_error) => match mqtt_error {
+                    ReasonCode::NetworkError => {
+                        println!("MQTT Network Error - Client Messsage Receive Error");
+                        // Consider retrying or reconnecting here
+                    }
+
+                    _ => {
+                        println!("Other MQTT Error: {:?}", mqtt_error);
+                        // Handle other errors
+                    }
+                },
+            }
+
+            Timer::after(Duration::from_millis(10000)).await;
+        }
+    }
 }
 
 #[embassy_executor::task]
 async fn connection(mut controller: WifiController<'static>) {
     println!("start connection task");
     println!("Device capabilities: {:?}", controller.get_capabilities());
+
     loop {
         match esp_wifi::wifi::get_wifi_state() {
             WifiState::StaConnected => {
@@ -238,6 +253,7 @@ async fn connection(mut controller: WifiController<'static>) {
                 auth_method: esp_wifi::wifi::AuthMethod::None,
                 ..Default::default()
             });
+
             controller.set_configuration(&client_config).unwrap();
             println!("Starting wifi");
             controller.start().await.unwrap();
